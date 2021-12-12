2786351e19235019769b55588080f2d75e385d1e
Anonymous Author <55631152+Anony123lyn@users.noreply.github.com>
2021-12-13 01:47:38 +0800

    Delete test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-#############     Source_only
-CUDA_VISIBLE_DEVICES=0 python tools/test_GTA_source_only.py --cfg experiments/GTA/LTIR_vgg_NEW.yaml TEST.TEST_FLIP True  VIS_DIR BB
-
-CUDA_VISIBLE_DEVICES=0 python tools/test_GTA_source_only.py --cfg experiments/GTA/LTIR_vgg_gta.yaml TEST.TEST_FLIP True VIS_DIR aa
-
-CUDA_VISIBLE_DEVICES=1 python tools/test_GTA_source_only.py --cfg experiments/GTA/Train_StoL_V2_1.yaml TEST.TEST_FLIP True
-
-
-CUDA_VISIBLE_DEVICES=0 python tools/test_GTA_source_only.py --cfg experiments/GTA/LTIR_vgg_gta.yaml TEST.TEST_FLIP True
