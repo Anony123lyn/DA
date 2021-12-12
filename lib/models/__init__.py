cc632ffe2de89d100c73008d9d827b5a31b6c4fc
Anonymous Author <55631152+Anony123lyn@users.noreply.github.com>
2021-12-13 01:46:41 +0800

    Delete lib directory

--- lib/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -------------------------------------------------------------------------------
-# Hybrid Resolution Network based Dual-Task Cascaded Framework for Human Parsing
-# Written by Lucas Zhao(liangzhao@njust.edu.cn)
-# -------------------------------------------------------------------------------
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
